8fac686276bc04efbde72ada671b83ab5a3b1fec
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-23 15:21:10 -0400

    crear db con json-server

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -1,7 +1,47 @@
 
 const EditarProducto = () => {
   return (
-    <h1>EditarProducto</h1>
+    <div className='row justify-content-center'>
+        
+    <div className='col-md-8'>
+        <div className="card">
+            <div className="card-body">
+                <h2 className='text-center mb-4 font-weight-bold'>
+                    Editar Producto
+                </h2>
+
+                <form>
+                    <div className='form-group'>
+                        <label htmlFor='nombre'>Nombre Producto</label>
+                        <input 
+                            type="text"
+                            className='form-control'
+                            placeholder='Nombre Producto' 
+                            name='nombre'
+                        />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='precio'>Precio Producto</label>
+                        <input 
+                            type="number"
+                            className='form-control'
+                            placeholder='Precio Producto' 
+                            name='precio'
+                        />
+                    </div>
+
+                    <button
+                        type='submit'
+                        className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+                    >Guardar Cambios</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
   )
 }
 
