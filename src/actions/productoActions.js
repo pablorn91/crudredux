7bbe7c15a3d352a393939639b68cf3fc7769e6a7
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-24 17:29:43 -0400

    descargar productos inicio

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,10 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types'
 
 import clienteAxios from '../config/axios'
@@ -58,4 +61,16 @@ const agregarProductoExito = producto => ({
 const agregarProductoError = estado => ({
     type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
+})
+
+//funcion que descarga los productos de la API
+export function obtenerProductosAction() {
+    return async dispatch => {
+        dispatch( descargarProductos() );
+    }
+}
+
+const descargarProductos = () => ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
 })
